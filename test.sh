c3de3552871bdff2b0f29242dad80b1305885e5f
Alex Martin <alexbuzzbee@gmail.com>
2015-05-29 19:05:44 -0400

    Added debugging plants to test script.

--- test.sh
+++ test.sh
@@ -2,10 +2,14 @@
 
 echo "In directory:"
 ls
+echo "Running UOP..."
 ./uop < testscript.txt > output.txt
 
+echo "Checking output..."
 if cmp --silent expected.txt output.txt; then
+  echo "Success!"
   exit 0
 else
+  echo "Failed!!!"
   exit 1
 fi
