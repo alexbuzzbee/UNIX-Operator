b9881f9cbad049c11f23fe6c105aa9cd286894c3
Alex Martin <alexbuzzbee@gmail.com>
2015-05-29 20:13:08 -0400

    Added menu library, which automatically displays menus, and refactored main.c to use it.

---
+++ lib/menu.c
@@ -0,0 +1,32 @@
+//UNIX Operator development version, simple menu-based interface for UNIX systems.
+//Copyright (C) 2014  Alex Martin
+
+//This program is free software: you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation, either version 3 of the License, or
+//(at your option) any later version.
+
+//This program is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//GNU General Public License for more details.
+
+//You should have received a copy of the GNU General Public License
+//along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+#include "menu.h"
+#include "util.h"
+
+int showMenu(char *prefix, char *title, char **options, size_t numOptions) {
+  int selection;
+  clear();
+  puts(prefix);
+  puts("---");
+  puts(title);
+  for (size_t i = 0; i < numOptions - 1; i++) {
+    printf("%i. %s\n", i+1, options[i]);
+  }
+  printf("Make a selection[1-%i]: ", numOptions);
+  scanf("%i", &selection);
+  return selection;
+}
--- main.c
+++ main.c
@@ -35,17 +35,7 @@ int mainMenu_opt_help() {
 int mainMenu_opt_about() {
   int selection;
   while(true) {
-    clear();
-    puts("About UNIX Operator");
-    puts("Version 0.3-dev by Alex Martin and GitHub contributors.");
-    puts("Copyright (C) 2014 Alex Martin.");
-    puts("This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.\n\nThis program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.");
-    puts("---");
-    puts("1. View GNU General Public License");
-    puts("2. View credits");
-    puts("3. Back");
-    printf("Make a selection[1,2,3]: ");
-    scanf("%i", &selection);
+    selection = showMenu("Version 0.3-dev by Alex Martin and GitHub contributors.\nCopyright (C) 2014 Alex Martin.\nThis program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.\n\nThis program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.\nUNIX Operator\nVersion 0.3-dev by Alex Martin and GitHub contributors\nCopyright (C) 2014 Alex Martin.", "About UNIX Operator", {"View GNU General Public License, Version 3", "View credits", "Back"});
     switch(selection) {
       case 1:
         util_viewFile("gpl-3.0.txt");
@@ -74,13 +64,7 @@ int apps_opt_cmdline() {
 
 int mainMenu_opt_apps() {
   int selection;
-  clear();
-  puts("Applications");
-  puts("1. UNIX Operator applications >");
-  puts("2. Command-line applications >");
-  puts("3. Back");
-  printf("Make a selection[1,2,3]: ");
-  scanf("%i", &selection);
+  showMenu("", "Applications", {"UNIX Operator applications >", "Command-line applications >", "Back"});
   switch(selection) {
     case 1:
       puts("Stub for UOP apps.");
@@ -102,14 +86,7 @@ void shutdown() {
 int mainMenu() {
   int selection;
   while(true) {
-    clear();
-    puts("UOP main menu");
-    puts("1. Help");
-    puts("2. About >");
-    puts("3. Applications >");
-    puts("4. Exit");
-    printf("Make a selection[1,2,3,4]: ");
-    scanf("%i", &selection);
+    selection = showMenu("", "UOP main menu", {"Help", "About >", "Applications >", "Exit"});
     switch(selection) {
       case 1:
         mainMenu_opt_help();
